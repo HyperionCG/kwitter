7e75609bb48951c829be40f0296995eea2fb9c01
Earth <earthhadjo@gmail.com>
2019-11-20 15:42:13 -0500

    updated register page created register component. added actions and reducers for function. next step is to link to profile after user registers

--- src/redux/actionCreators/auth.js
+++ src/redux/actionCreators/auth.js
@@ -1,14 +1,15 @@
 import { domain, jsonHeaders, handleJsonResponse } from "./constants";
-import { LOGIN, LOGOUT } from "../actionTypes";
+import { LOGIN, LOGOUT, REGISTER } from "../actionTypes";
 
-const url = domain + "/auth";
+
+const url = domain ;
 
 export const login = loginData => dispatch => {
   dispatch({
     type: LOGIN.START
   });
 
-  return fetch(url + "/login", {
+  return fetch(url + "/auth/login", {
     method: "POST",
     headers: jsonHeaders,
     body: JSON.stringify(loginData)
@@ -32,7 +33,7 @@ export const logout = () => (dispatch, getState) => {
 
   const token = getState().auth.login.result.token;
 
-  return fetch(url + "/logout", {
+  return fetch(url + "/auth/logout", {
     method: "GET",
     headers: { Authorization: "Bearer " + token, ...jsonHeaders }
   })
@@ -47,5 +48,28 @@ export const logout = () => (dispatch, getState) => {
       return Promise.reject(
         dispatch({ type: LOGOUT.FAIL, payload: err.message })
       );
+    });
+};
+
+
+export const register = registerData => dispatch => {
+  dispatch({
+    type: REGISTER.START
+  });
+
+  return fetch(url + "/users", {
+    method: "POST",
+    headers: jsonHeaders,
+    body: JSON.stringify(registerData)
+  })
+    .then(handleJsonResponse)
+    .then(result => {
+      return dispatch({
+        type: REGISTER.SUCCESS,
+        payload: result
+      });
+    })
+    .catch(err => {
+      return Promise.reject(dispatch({ type: REGISTER.FAIL, payload: err }));
     });
 };
